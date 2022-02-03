79479d20007dafd524944ebdf23e7a6a97949546
llcc01 <2417224420@qq.com>
2022-02-03 17:39:57 +0800

    添加测试环境

--- config/config.go
+++ config/config.go
@@ -7,12 +7,15 @@ const CORPID string = "ww8a5308483ff283cc"
 const CORPSECRET string = "EPQstC4qi51TcvtVQRzQ1HowUdJ4jrOG_cFgcIA160E"
 
 //数据库用户名密码
-const DB_USER_PW string = "squidward_top:789456"
+const DB_USER_PW string = "ddl_test:ddl_test"
 
 //数据库主机
 // const DB_HOST string = "lllccc.top:20570"
 
 const DB_HOST string = "localhost"
+
+//数据库名
+const DB string = "ddl_test"
 
 //测试密钥，实际环境请修改
 const ENCRYPT_KEY string = "1234567890abcdef"
@@ -22,3 +25,7 @@ var AdminKey = map[string]int64{
 	"admin":    267387135,
 	"12345678": 0,
 }
+
+const WEB_ADDR string = ":30004"
+
+const UPLOAD_PATH string = "./pic"
--- database/database.go
+++ database/database.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func Open() (err error) {
-	DB, err = gorm.Open("mysql", config.DB_USER_PW+"@("+config.DB_HOST+")/test?charset=utf8&loc=Local&parseTime=true")
+	DB, err = gorm.Open("mysql", config.DB_USER_PW+"@("+config.DB_HOST+")/"+config.DB+"?charset=utf8&loc=Local&parseTime=true")
 	DB.LogMode(true) //开启sql debug 模式
 	return err
 }
